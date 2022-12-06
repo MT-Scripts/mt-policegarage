b4944e3647f2b68051a7fb24975129f833640a0e
Marttins <89866234+Marttins011@users.noreply.github.com>
2022-12-06 17:35:41 +0000

    Full rework
    - Added option at config to add how many locations you want
    - You can lock vehicles to some grades
    - Better vehicle spawn
    - Better vehicle delete

---
+++ mt-policegarage/config.lua
@@ -0,0 +1,14 @@
+Config = Config or {}
+
+Config.Locations = {
+    [1] = {
+        ['Coords'] = vector3(381.59, -1625.03, 29.29),
+        ['Heading'] = 100,
+        ['SpawnCoords'] = vector4(381.59, -1625.03, 29.29, 100),
+        ['Job'] = 'police',
+        ['Vehicles'] = {
+            [1] = { VehicleName = 'Police 1', VehicleSpawnName = 'police', Grade = 0 },
+            [2] = { VehicleName = 'Police 2', VehicleSpawnName = 'police2', Grade = 1 },
+        },
+    },
+}
